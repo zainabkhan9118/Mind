e16fbba260d354f331765eff7b76d275f1ba6b1f
Zainab Iqbal <124600284+zainabkhan9118@users.noreply.github.com>
2025-09-12 17:57:30 +0500

    fixed the gap

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -6,10 +6,12 @@ import Benefits from "@/components/sections/home/Benefits";
 
 const ExperiencePage = () => {
   return (
-    <div > 
+    <div> 
       <ExperienceHero />
       <ExperienceCards />
-      <Benefits />
+      <div className="mt-[-60px] sm:mt-[-80px] md:mt-[-140px] lg:mt-[-200px] xl:mt-[-180px] 2xl:mt-[-200px] 3xl:mt-[-220px] 4xl:mt-[-240px]">
+        <Benefits />
+      </div>
       <Reason />
 
     </div>
